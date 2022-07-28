b3200df093f438224563c4334c5c9b7040c2fcf0
merve-burakkagan-evleniyor <109975992+merve-burakkagan-evleniyor@users.noreply.github.com>
2022-07-28 16:27:35 +0300

    initial commit

---
+++ js/countdown.js
@@ -0,0 +1,28 @@
+// Event date to count down from
+var eventTime = new Date("Sep 11, 2022, 19:30:00").getTime();
+
+// Counting down
+var x = setInterval(function() {
+    // Some constants for time calculations
+    var dayFactor = 1000*60*60*24;
+    var hourFactor = dayFactor/24;
+    var minuteFactor = hourFactor/60;
+    var secondFactor = minuteFactor/60;
+    
+    // Distance between the even time and now
+    var currentTime = new Date().getTime();
+    var distance = eventTime - currentTime;
+    
+    // Time calculations
+    var daysLeft = Math.floor(distance/dayFactor);
+    var hoursLeft = Math.floor((distance%dayFactor)/hourFactor);
+    var minutesLeft = Math.floor((distance%hourFactor)/minuteFactor);
+    var secondsLeft = Math.floor((distance%minuteFactor)/secondFactor);
+    
+    document.getElementById("time-left").innerHTML = daysLeft + "g " + hoursLeft + "s " + minutesLeft + "dk " + secondsLeft + "sn";
+    
+    if (distance<0) {
+        clearInterval(x);
+        document.getElementById("time-left").innerHTML = "EXPIRED";
+    }
+}, 1000);
